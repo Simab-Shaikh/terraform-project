f0cb73a07064ccb48b07fc3ec5ce485769f6db4b
Simab-Shaikh <simabshaikh100@gmail.com>
2026-01-06 23:31:03 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,1 +1,10 @@
+resource  "aws_instance" "web" {
+   ami             = var.ami_id
+   instance_type   = var.instance_type
+   subnet_id       = aws_subnet.pulic.id
+
+   tags = {
+     Name = "terraform-ec2"
+  }
+}
 
