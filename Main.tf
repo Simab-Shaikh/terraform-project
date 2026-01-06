2f861baa391487659017857ba2b45a24a241dad7
Simab-Shaikh <simabshaikh100@gmail.com>
2026-01-07 00:24:55 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -1,1 +1,12 @@
+module "vpc" {
+ source = "./modules/vpc"
+ cidr = "10.0.0.0/16"
+}
 
+
+module "ec2" {
+ source = "./modules/ec2"
+ ami = var.ami_id
+ type = var.instance_type
+ subnet_id = module.vpc.public_subnet_id
+}
